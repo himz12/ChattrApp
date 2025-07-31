7ba193dbcfa273174639a462d0aa36045f5ed762
Himanshi Negi <himanshinegi401@gmail.com>
2025-07-31 18:45:35 +0530

    Fix: wildcard route issue for Render deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(frontendPath));
 
   // Fixed wildcard route
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(frontendPath, 'index.html'));
   });
 }
